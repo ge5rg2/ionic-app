e56b1d52ddadd428d7da053da4f5d6254c5e75f9
ge5rg2 <kghee9612@gmail.com>
2023-06-09 09:21:45 +0900

    feat: add scrollPosition state

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -5,19 +5,37 @@ import {
   IonTitle,
   IonToolbar,
 } from "@ionic/react";
-import ExploreContainer from "../components/ExploreContainer";
 import "./Tab1.css";
+import { useState } from "react";
 
 const Tab1: React.FC = () => {
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  const handleScrollLeft = () => {
+    setScrollPosition((prevPosition) => prevPosition - 25);
+  };
+
+  const handleScrollRight = () => {
+    setScrollPosition((prevPosition) => prevPosition + 25);
+  };
+
   return (
     <IonPage>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        <div className="panorama-container">
+          <div
+            className="panorama-image"
+            style={{ transform: `translateX(-${scrollPosition}%)` }}
+          >
+            <img src="./public/img/m1.jpg" alt="Panorama" />
+          </div>
+          <div className="panorama-button left" onClick={handleScrollLeft}>
+            <img src={""} alt="Left Arrow" />
+          </div>
+          <div className="panorama-button right" onClick={handleScrollRight}>
+            <img src={""} alt="Right Arrow" />
+          </div>
+        </div>
       </IonContent>
     </IonPage>
   );
